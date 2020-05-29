961757168d001f4ec683b969156065f158aa6f1e
Soniasonia6 <43379274+Soniasonia6@users.noreply.github.com>
2020-05-29 11:54:43 -0400

    first commit

---
+++ HelloWorld.js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+//Require the http module so we can send requests
+var http = require('http');
+//create our webserver that listens on port 3000 of localhost/127.0.01
+http.createServer(function (req, res) {
+    //tell our server, hey we are ok and good to go, expect some text!
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    //end the request with some text
+    res.end('Hello World');
+    //listen on port 3000
+}).listen(3000);
+console.log('Server listening on port 3000');
+
